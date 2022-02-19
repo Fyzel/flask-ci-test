bed90fc682d549e264b4b850f6bddb69829821b8
Fyzel <Fyzel@users.noreply.github.com>
2022-02-18 17:35:53 -0700

    Move pylint to python-app-tests

--- tests/functional/test_functions.py
+++ tests/functional/test_functions.py
@@ -19,4 +19,4 @@ def test_home_page():
     with flask_app.test_client() as test_client:
         response = test_client.get('/')
         assert response.status_code == 200
-        assert b"Hello world" in response.data
+        assert b"Hello, World!" in response.data
